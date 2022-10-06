d9de9226b861ea0c2accbbe175af4daffe4e10c9
AwkwardMongoose <82236528+AwkwardMongoose@users.noreply.github.com>
2022-10-06 14:29:05 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
 console.log('Loaded')
+
+const tabRadios = $(".tabradio").toArray();
+const tabSwitches = $(".tabswitch").toArray();
+
+
+
+tabRadios.forEach(tab => {
+    tab.addEventListener('change', function() {
+        tabSwitches.forEach(swit => {
+            if (swit.id != tab.id) {
+                swit.checked = false;
+            } else {
+                swit.checked = true;
+            }
+        })
+    })
+})
